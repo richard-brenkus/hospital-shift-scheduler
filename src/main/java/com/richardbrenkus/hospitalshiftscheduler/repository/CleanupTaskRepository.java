7646408c5c382ca4e62e0a7df01ad79422a90cff
richard-brenkus <richard_brenkus@yahoo.com>
2026-08-16 15:34:04 +0200

    messages properties cleanup

--- src/main/java/com/richardbrenkus/hospitalshiftscheduler/repository/CleanupTaskRepository.java
+++ src/main/java/com/richardbrenkus/hospitalshiftscheduler/repository/CleanupTaskRepository.java
@@ -6,16 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface CleanupTaskRepository extends JpaRepository<CleanupTask, Long> {
-
-    //Optional<CleanupTask> findById(Long id);
-
+    
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("""
        select task
